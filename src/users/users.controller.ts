bd375f68bbfd60d47fda26a17f23ab51fca2686a
4lerman <almaserbolat60@gmail.com>
2023-05-16 20:00:20 +0600

    added decorator for getting user with parameter and patch method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { GetUser } from 'src/auth/decorator/get_user.decorator';
+import { User } from '@prisma/client';
+import { EditUserDto } from './dto/edit-user.dto';
 
-@Controller('users')
+@UseGuards(JwtGuard)
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  getUser(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.usersService.editUser(userId, dto);
+  }
 }
